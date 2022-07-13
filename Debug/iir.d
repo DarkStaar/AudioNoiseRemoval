d89a769292ee4db69443d2fe5571c9d4702b8501
Milan Kapetanovic <63914173+DarkStaar@users.noreply.github.com>
2022-07-13 23:32:09 +0200

    Final commit

---
+++ Debug/iir.d
@@ -0,0 +1,9 @@
+# FIXED
+
+iir.obj: ../iir.c
+iir.obj: ../iir.h
+iir.obj: C:/Users/Milan/workspace_v6_1_3/Vezba1-Zadatak1/c55xx_csl/inc/tistdtypes.h
+
+../iir.c: 
+../iir.h: 
+C:/Users/Milan/workspace_v6_1_3/Vezba1-Zadatak1/c55xx_csl/inc/tistdtypes.h: 
